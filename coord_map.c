d9e99cc0cba97dc7394a8cbe4186b819b0f01f8a
detomon <simon@monoxid.ch>
2017-07-21 17:37:58 +0200

    Initial commit

---
+++ coord_map.c
@@ -0,0 +1,139 @@
+#include <string.h>
+#include "coord_map.h"
+
+#define MIN_SIZE 33
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+static inline size_t size_for_coord(int a) {
+	return ABS(a) * 2 + 1;
+}
+
+static inline int coord_in_list(int a, size_t size) {
+	return size_for_coord(a) < size;
+}
+
+static int coord_map_resize(coord_map* map, int x) {
+	size_t size = size_for_coord(x);
+	size_t old_size = map->size;
+	size_t new_size = old_size;
+	coord_axis** coords = map->coords;
+
+	while (new_size < size) {
+		new_size = MAX(MIN_SIZE, new_size * 2 + 1);
+	}
+
+	coords = realloc(coords, sizeof(coord_axis*) * new_size);
+
+	if (!coords) {
+		return -1;
+	}
+
+	if (old_size) {
+		size_t hdiff = (new_size - old_size) / 2;
+
+		memmove(&coords[hdiff], coords, sizeof(coord_axis*) * old_size);
+		memset(&coords[0], 0, sizeof(coord_axis*) * hdiff);
+		memset(&coords[new_size - hdiff], 0, sizeof(coord_axis*) * hdiff);
+	}
+	else {
+		memset(coords, 0, sizeof(coord_axis*) * new_size);
+	}
+
+	map->size = new_size;
+	map->coords = coords;
+
+	return 0;
+}
+
+static coord_axis* coord_map_resize_axis(coord_axis** axis_ref, int y) {
+	coord_axis* axis = *axis_ref;
+	size_t size = size_for_coord(y);
+	size_t old_size = axis ? axis->size : 0;
+	size_t new_size = old_size;
+	size_t const type_size = sizeof(typeof(axis->items[0]));
+
+	while (new_size < size) {
+		new_size = MAX(MIN_SIZE, new_size * 2 + 1);
+	}
+
+	axis = realloc(axis, sizeof(coord_axis) + type_size * new_size);
+
+	if (!axis) {
+		return NULL;
+	}
+
+	if (old_size) {
+		size_t hdiff = (new_size - old_size) / 2;
+
+		memmove(&axis->items[hdiff], &axis->items, type_size * old_size);
+		memset(&axis->items[0], 0, type_size * hdiff);
+		memset(&axis->items[new_size - hdiff], 0, type_size * hdiff);
+	}
+	else {
+		memset(axis->items, 0, type_size * new_size);
+	}
+
+	axis->size = new_size;
+	*axis_ref = axis;
+
+	return axis;
+}
+
+int coord_map_init(coord_map* map) {
+	*map = (coord_map) {
+		.size = 0,
+	};
+
+	return 0;
+}
+
+int coord_map_get(coord_map* map, int x, int y) {
+	if (coord_in_list(x, map->size)) {
+		size_t cx = map->size / 2;
+		coord_axis* axis = map->coords[cx + x];
+
+		if (axis && coord_in_list(y, axis->size)) {
+			size_t cy = axis->size / 2;
+
+			return axis->items[cy + y];
+		}
+	}
+
+	return 0;
+}
+
+int coord_map_set(coord_map* map, int x, int y, int value) {
+	size_t cx;
+	size_t cy;
+	coord_axis* axis;
+
+	if (!coord_in_list(x, map->size)) {
+		if (coord_map_resize(map, x) != 0) {
+			return -1;
+		}
+
+	}
+
+	cx = map->size / 2;
+	axis = map->coords[cx + x];
+
+	if (!axis || !coord_in_list(y, axis->size)) {
+		if (!(axis = coord_map_resize_axis(&map->coords[cx + x], y))) {
+			return -1;
+		}
+	}
+
+	cy = axis->size / 2;
+	axis->items[cy + y] = value;
+
+	return 0;
+}
+
+void coord_map_free(coord_map* map) {
+	for (size_t i = 0; i < map->size; i++) {
+		free(map->coords[i]);
+	}
+
+	free(map->coords);
+}
---
+++ coord_map.h
@@ -0,0 +1,92 @@
+/*
+ * This map is a dynamically growing 2-dimensional array.
+ * Values can be set at arbitary x/y-coordinates, either positive or negative.
+ *
+ * ## Example
+ *
+ * ```c
+ * coord_map map;
+ *
+ * // initialize map
+ * coord_map_init(&map);
+ *
+ * // set values at given coordinates
+ * coord_map_set(&map, 12, -6, 1);
+ * coord_map_set(&map, 12, 8, 2);
+ * coord_map_set(&map, 400, 3000, 3);
+ * coord_map_set(&map, 5000, -1000, 4);
+ *
+ * // get values form given coordinates
+ * int a = coord_map_get(&map, 12, -6);
+ * int b = coord_map_get(&map, 12, 8);
+ * int c = coord_map_get(&map, 400, 3000);
+ * int d = coord_map_get(&map, 5000, -1000);
+ * int e = coord_map_get(&map, -150, 40); // not set = 0
+ *
+ * // print values: 1, 2, 3, 4, 0
+ * printf("%d %d %d %d %d\n", a, b, c, d, e);
+ *
+ * // free map
+ * coord_map_free(&map);
+ * ```
+ */
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+
+/**
+ * Defines values of an x-coordinate.
+ */
+typedef struct {
+	size_t size; ///< The number of y-coordinates.
+	int items[]; ///< The values of the y-coordinates.
+} coord_axis;
+
+/**
+ * The map which holds the values.
+ */
+typedef struct {
+	size_t size;         ///< The number of y-coordinates.
+	coord_axis** coords; ///< The x-coordinates.
+} coord_map;
+
+/**
+ * Initialize map.
+ *
+ * @param map The map to initialize.
+ *
+ * @return 0 on success.
+ */
+extern int coord_map_init(coord_map* map);
+
+/**
+ * Free map.
+ *
+ * @param map The map to free.
+ */
+extern void coord_map_free(coord_map* map);
+
+/**
+ * Get value at given coordinate.
+ *
+ * @param map The map to add the value.
+ * @param x The x-coordinate.
+ * @param y The y-coordinate.
+ *
+ * @return The value at the speciafied coordinates or 0 if not found.
+ */
+extern int coord_map_get(coord_map* map, int x, int y);
+
+/**
+ * Set value at given coordinate.
+ *
+ * @param map The map to add the value
+ * @param x The x-coordinate.
+ * @param y The y-coordinate.
+ * @param value The value to set at the specified coordinates.
+ *
+ * @return 0 on success.
+ */
+extern int coord_map_set(coord_map* map, int x, int y, int value);
